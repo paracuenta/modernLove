b64130bb09d4106c9062b4091a451c5a93931f3c
Aitor García <Aitor Garca>
2017-02-03 16:21:47 +0100

    Added pages to left menu

--- src/pages/single/single.ts
+++ src/pages/single/single.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Single page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-single',
   templateUrl: 'single.html'
 })
 export class SinglePage {
 
+  name1 ="";
+  name2 ="";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SinglePage');
+  }
+
+  get score() {
+    const letters =(this.name1 + this.name2).toLowerCase();
+    let sum = 0;
+    for (let i=0; i<letters.length; i++){
+      sum += letters.charCodeAt(i);
+    }
+    return sum % 101;
   }
 
 }
