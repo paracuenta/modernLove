0c1527bc72aa56c9fee7eb80a53f7e6f019c7e33
Aitor García <Aitor Garca>
2017-02-06 23:52:51 +0100

    translations in slides

--- src/pages/slides/slides.ts
+++ src/pages/slides/slides.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { TranslateService } from 'ng2-translate';
+
 import { MainPage } from '../main/main';
 
 @Component({
@@ -10,25 +12,37 @@ import { MainPage } from '../main/main';
 export class SlidesPage {
   slides = [
     {
-      title: "!Bienvenid@ a Modern Love!",
-      description: "La app que te permitirá saber la <b>compatibilidad con otras personas</b>.",
       image: "assets/img/ica-slidebox-img-1.png",
     },
     {
-      title: "¿Tu pareja ideal?",
-      description: "¿Estás <b>enamorad@</b> de tu pareja? Introduce vuestros nombres y descubre si es la definitiva. O puede que descubras nuevos amores de una forma que nunca habías imaginado.",
       image: "assets/img/ica-slidebox-img-2.png",
     },
     {
-      title: "¿Sol@ o en compañía?",
-      description: "¿Quién te va a querer mas que tu mismo? ¿Alguna vez has imaginado estar con más de una persona... <b>al mismo tiempo</b>? Averígualo!",
       image: "assets/img/ica-slidebox-img-3.png",
     }
   ];
-
   rootPage: any = MainPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, translate: TranslateService) {
+    translate.get('TITLESLIDE1').subscribe((res: string) => {
+      this.slides[0]['title'] = res;
+    });
+    translate.get('DESCRIPTIONSLIDE1').subscribe((res: string) => {
+      this.slides[0]['description'] = res;
+    });
+    translate.get('TITLESLIDE2').subscribe((res: string) => {
+      this.slides[1]['title'] = res;
+    });
+    translate.get('DESCRIPTIONSLIDE2').subscribe((res: string) => {
+      this.slides[1]['description'] = res;
+    });
+    translate.get('TITLESLIDE3').subscribe((res: string) => {
+      this.slides[2]['title'] = res;
+    });
+    translate.get('DESCRIPTIONSLIDE3').subscribe((res: string) => {
+      this.slides[2]['description'] = res;
+    });
+  }
   
   afterSlides(){
     this.navCtrl.setRoot(MainPage);
